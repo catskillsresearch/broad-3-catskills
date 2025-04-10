53120aff7f8ba43bff21d55c80a79ea7af2de40d
Lars Ericson <lars.ericson@catskillsresearch.com>
2025-04-10 07:43:18 -0400

    crunchy

--- crunch-1.py
+++ crunch-1.py
@@ -5,7 +5,7 @@ from infer1 import *
 # /!\ Don't forget to import pre-trained Resnet50 "pytorch_model.bin" in model_directory_path="./resources" from https://huggingface.co/timm/resnet50.tv_in1k/tree/main
 #train1(data_directory_path='./data.2.large', model_directory_path="./resources")
 prediction = infer1(
-    data_file_path="./data/UC1_NI.zarr",
+    data_file_path="./data/UC9_I.zarr",
     model_directory_path="./resources"
 )
 print(prediction.head())
