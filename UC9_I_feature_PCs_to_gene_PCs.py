1d08a0e21f9690374919b5bbd0877ed24a4e16c5
Lars Ericson <lars.ericson@catskillsresearch.com>
2025-04-22 22:42:00 -0400

    better

--- UC9_I_feature_PCs_to_gene_PCs.py
+++ UC9_I_feature_PCs_to_gene_PCs.py
@@ -19,34 +19,39 @@ class UC9_I_feature_PCs_to_gene_PCs(luigi.Task):
             'mse': luigi.LocalTarget(f'resources/run/UC9_I_feature_PCs_to_gene_PCs_mse.txt'),
             'prediction': luigi.LocalTarget(f'resources/run/UC9_I_feature_PCs_to_gene_PCs_pred.png'),
             'spearman': luigi.LocalTarget(f'resources/run/UC9_I_feature_PCs_to_gene_PCs_spearman.png')}
-        
-    def run(self):
-        feature_PCs = np.load(self.input()['features']['PCs'].path)['arr_0']
-        gene_PCs = np.load(self.input()['genes458']['PCs'].path)['arr_0']
-        W = ridge_fit(feature_PCs, gene_PCs)
-        np.savez_compressed(self.output()['W'].path, W)
 
-        # Metrics
-        Y_hat = ridge_apply(feature_PCs, W)
-        
-        ## MSE
-        mse = mean_squared_error(gene_PCs, Y_hat)
+    def mse_metric(self):
+        mse = mean_squared_error(self.gene_PCs, self.Y_hat)
         with open(self.output()['mse'].path,'w') as f:
             print(mse, file=f)
-        plt.plot(Y_hat[0], label='Yhat', color='green')
-        plt.plot(gene_PCs[0], label='Y', color='blue')
+        plt.plot(self.Y_hat[0], label='Yhat', color='green')
+        plt.plot(self.gene_PCs[0], label='Y', color='blue')
         plt.title('Yhat[0] vs Y[0] for imputed gene PCs on trained Ridge regression')
         plt.legend()
         plt.savefig(self.output()['prediction'].path, dpi=150, bbox_inches='tight')
-        
-        ## Spearman rank correlation
-        correlations = [spearmanr(a_row, b_row)[0] for a_row, b_row in zip(gene_PCs, Y_hat)]
+        plt.clf()
+
+    def spearman_metric(self):
+        correlations = [spearmanr(a_row, b_row)[0] for a_row, b_row in zip(self.gene_PCs, self.Y_hat)]
         mu = np.mean(correlations)
         plt.hist(correlations, density=True, bins=100);
         plt.axvline(x=mu, color='r', linestyle='--', alpha=0.7)
         plt.title(f"""Spearman rank correlation density between predicted and actual
         Mean={mu:0.3f}""");
         plt.savefig(self.output()['spearman'].path, dpi=150, bbox_inches='tight')
+        plt.clf()
+        
+    def run(self):
+        # Regression
+        feature_PCs = np.load(self.input()['features']['PCs'].path)['arr_0']
+        self.gene_PCs = np.load(self.input()['genes458']['PCs'].path)['arr_0']
+        W = ridge_fit(feature_PCs, self.gene_PCs)
+        np.savez_compressed(self.output()['W'].path, W)
+
+        # Metrics
+        self.Y_hat = ridge_apply(feature_PCs, W)
+        self.mse_metric()
+        self.spearman_metric()
     
 if __name__ == "__main__":
     luigi.build(
