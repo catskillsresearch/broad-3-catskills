f549294129033af03b392fa40367b9d70e92e67a
Lars Ericson <lars.ericson@catskillsresearch.com>
2025-04-27 13:09:31 -0400

    nearness

--- dysplasia_feature_PCs_to_gene_PCs.py
+++ dysplasia_feature_PCs_to_gene_PCs.py
@@ -1,18 +1,17 @@
 import os, luigi
 from template_ridge_transform import template_ridge_transform
-from dusfeatures_to_PCs import UC9_I_features_to_PCs
-from UC9_I_genes458_to_PCs import UC9_I_genes458_to_PCs
+from dysplasia_features_to_PCs import dysplasia_features_to_PCs
+from UC9_I_features_to_PCs import UC9_I_features_to_PCs
+from UC9_I_feature_PCs_to_gene_PCs import UC9_I_feature_PCs_to_gene_PCs
 
 class dysplasia_feature_PCs_to_gene_PCs(luigi.Task):
     def requires(self):
         # Provide hardcoded parameters and dependency
         return template_ridge_transform(
-            src_object_type = "feature_PCs",
-            src_object_name = "UC9_I_dysplasia",
             src_task = UC9_I_features_to_PCs,
+            fit_task = UC9_I_feature_PCs_to_gene_PCs,
             tgt_object_type = "genes458_PCs",
-            tgt_object_name = "UC9_I_dysplasia", 
-            tgt_task = UC9_I_genes458_to_PCs)
+            tgt_object_name = "UC9_I_dysplasia")
 
     def run(self):
         pass
--- dysplasia_features_to_PCs.py
+++ dysplasia_features_to_PCs.py
@@ -1,18 +1,18 @@
 import os, luigi
-from template_pca_fit_transform import template_pca_fit_transform
-from dysplasia_patches_to_features import dysplasia_patches_to_features
+from template_pca_transform import template_pca_transform
+from UC9_I_genes458_to_PCs import UC9_I_genes458_to_PCs
+from UC9_I_genes460_to_genes458 import UC9_I_genes460_to_genes458
+from SCRNA_unpack import SCRNA_unpack
 
 class dysplasia_features_to_PCs(luigi.Task):
     def requires(self):
         # Provide hardcoded parameters and dependency
-        return template_pca_fit_transform(
+        return template_pca_transform(
             object_type="feature",
             object_name="UC9_I_dysplasia",
-            mse_goal=0.16,
-            dependency_task=dysplasia_patches_to_features,
-            sub_input = None,
-            sample_size = 1000
-        )
+            pca_fit_transform = UC9_I_genes458_to_PCs,
+            source = SCRNA_unpack,
+            source_field = 'scRNA_458_gene_expressions')
 
     def run(self):
         pass
