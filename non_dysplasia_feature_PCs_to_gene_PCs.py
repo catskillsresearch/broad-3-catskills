4daca24ef7943ba9b48c50355699561100258bfe
Lars Ericson <lars.ericson@catskillsresearch.com>
2025-04-27 18:58:18 -0400

    closer

---
+++ non_dysplasia_feature_PCs_to_gene_PCs.py
@@ -0,0 +1,26 @@
+import os, luigi
+from template_ridge_transform import template_ridge_transform
+from non_dysplasia_features_to_PCs import non_dysplasia_features_to_PCs
+from UC9_I_feature_PCs_to_gene_PCs import UC9_I_feature_PCs_to_gene_PCs
+
+class non_dysplasia_feature_PCs_to_gene_PCs(luigi.Task):
+    def requires(self):
+        # Provide hardcoded parameters and dependency
+        return template_ridge_transform(
+            src_task = non_dysplasia_features_to_PCs,
+            fit_task = UC9_I_feature_PCs_to_gene_PCs,
+            tgt_object_type = "genes458_PCs", 
+            tgt_object_name = "UC9_I_non_dysplasia")
+
+    def run(self):
+        pass
+
+    def output(self):
+        return self.requires().output()
+    
+if __name__ == "__main__":
+    luigi.build(
+        [non_dysplasia_feature_PCs_to_gene_PCs()],
+        local_scheduler=True,  # Required for local execution
+        workers=1  # Optional: single worker for serial execution
+    )
